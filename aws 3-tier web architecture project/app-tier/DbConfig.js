bd1edb463d775915b16b4ad220f39abe1708a1d6
Prashanth Sankar <144333920+Prasu-1899@users.noreply.github.com>
2024-07-03 17:25:08 +0530

    Add files via upload

---
+++ aws 3-tier web architecture project/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.cls93gl28vgb.ap-southeast-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'admin123',
+    DB_DATABASE : 'webappdb'
+});
